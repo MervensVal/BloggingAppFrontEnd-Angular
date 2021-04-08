843bff244070e6489f4847a3adf0260c1d95d7e5
Mervens Val <valmervens@gmail.com>
2021-04-07 20:50:00 -0400

    Added nav routes and forms for register and login

--- Assignment4/BloggingApp/src/app/app-routing.module.ts
+++ Assignment4/BloggingApp/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorrouteComponent } from './views/errorroute/errorroute.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //default path
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'**',
+    component:ErrorrouteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Assignment4/BloggingApp/src/app/app.module.ts
+++ Assignment4/BloggingApp/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { ErrorrouteComponent } from './views/errorroute/errorroute.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent,
+    ErrorrouteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
