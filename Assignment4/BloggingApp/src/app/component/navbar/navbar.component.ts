0e88a1b2faf44afc962638c168f35d31141847c7
Mervens Val <valmervens@gmail.com>
2021-04-17 21:23:44 -0400

    Added Logout feature

--- Assignment4/BloggingApp/src/app/component/navbar/navbar.component.ts
+++ Assignment4/BloggingApp/src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Token } from 'src/app/models/token.model';
+import { UserserviceService } from 'src/app/services/userservice.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  userIsLoggedIn= false;
+  currentUser: Token|null=null;
+  constructor(private userSvc: UserserviceService, private router:Router) {
 
+    let userLoggedIn = this.userSvc.GetLoggedInUser();
+    if(userLoggedIn!=null)
+    {
+      this.userIsLoggedIn = true;
+      this.currentUser = userLoggedIn;
+    }
+    this.userSvc.UserStateChanged.subscribe((userLoggedInMsg)=>{
+      this.userIsLoggedIn = userLoggedInMsg!==null;
+      this.currentUser = userLoggedIn;
+    })
+  }
   ngOnInit(): void {
   }
 
+  LogoutUser()
+  {
+    this.userSvc.SetUserAsLoggedOff();
+    this.userIsLoggedIn=false;
+    this.router.navigate(['/login']);
+  }
 }
